3b98b8658c8bb6fcb106659f52f3af271bbbff62
gilmarperonjunior <109175794+DevJuniorP@users.noreply.github.com>
2023-05-04 09:00:58 -0300

    organograma
    04/05/2023

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner() {
+   return (
+      <header className="banner"> 
+         <img src="/images/banner.png" alt="Banner Principal da Página"/>
+      </header>
+   )
+}
+
+export default Banner
