6fc3a27f261733e1ebd6127c4e3a7452f78fe49b
gilmarperonjunior <109175794+DevJuniorP@users.noreply.github.com>
2023-05-23 10:53:06 -0300

    organo
    2305

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,16 @@
 import './time.css'
 
 const Time = (props) => {
+    const backgrounds = {backgroundColor: props.corSecundaria}
+    const border = {borderColor: props.corPrimaria}
+
+    // caso eu queira fazer estilo inline ficaria 
+    // <section className='time' style={{ backgroundColor: props.corPrimaria }}>
+    // o mesmo vale pra borda
+
     return(
-        <section className='time'>
-            <h3>{props.nome}</h3>
+        <section className='time' style={backgrounds}>
+            <h3 style={border}>{props.nome}</h3>
         </section>
     )
 }
